8e4e002b13137f3e48558928083f7985b8d99816
Andrei Vauchok <vauchokandrei@gmail.com>
2024-02-10 20:11:17 +0300

    feat: add fs operations

---
+++ src/utils/errors.js
@@ -0,0 +1,6 @@
+export const errors = {
+  INVALID_INPUT: "Invalid input",
+  PATH_REQUIRED: "Path is required",
+  FILENAME_REQUIRED: "File name not specified",
+  FILE_REQUIRED: "Specify a file",
+};
